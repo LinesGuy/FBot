0287fea2e26cee4a949f8ca62e1a6212542d1e8b
Line <gudgeon.christopher@gmail.com>
2023-04-26 15:58:46 +0100

    Add automagic error reporting

--- extensions/error_handler.py
+++ extensions/error_handler.py
@@ -1,3 +1,4 @@
+from traceback import format_exception
 from discord.ext import commands
 
 class Errorhandler(commands.Cog):
@@ -12,6 +13,15 @@ class Errorhandler(commands.Cog):
                 return
             case commands.NotOwner:
                 await ctx.reply("Only bot owners can run this command!")
+            case _:
+                await ctx.send("An unusual error has occured, the devs have been notified.")
+
+                error_msg  = f"Error on message: `{ctx.message.content}`\n"
+                error_msg += f"Author: `{ctx.message.author}`\nid: `{ctx.message.author.id}`\n"
+                error_msg += f"Guild: `{ctx.message.guild}`\nid: `{ctx.message.guild.id}`\n"
+                error_msg += f"```{format_exception(error, error, error.__traceback__)}```"
+                await self.bot.get_channel(727320090483359844).send(error_msg)
+
 
 async def setup(bot):
     await bot.add_cog(Errorhandler(bot))
