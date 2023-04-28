fe5f2db27603c3245c91a2a808fe4fd5d066a6f6
Line <gudgeon.christopher@gmail.com>
2023-04-28 17:59:31 +0100

    send error message as .txt file

--- extensions/error_handler.py
+++ extensions/error_handler.py
@@ -1,5 +1,7 @@
 from traceback import format_exception
 from discord.ext import commands
+import discord
+import io
 
 class Errorhandler(commands.Cog):
 
@@ -20,7 +22,9 @@ class Errorhandler(commands.Cog):
                 error_msg += f"Author: `{ctx.message.author}`\nid: `{ctx.message.author.id}`\n"
                 error_msg += f"Guild: `{ctx.message.guild}`\nid: `{ctx.message.guild.id}`\n"
                 error_msg += f"```{format_exception(error, error, error.__traceback__)}```"
-                await self.bot.get_channel(727320090483359844).send(error_msg)
+                f = io.StringIO(error_msg)
+                f.name = "error_report.txt"
+                await self.bot.get_channel(727320090483359844).send("New error report just dropped:", file=discord.File(f))
 
 
 async def setup(bot):
