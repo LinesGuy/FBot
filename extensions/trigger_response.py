14b9327bba029eaec3498068e8f985d73538c94c
Line <gudgeon.christopher@gmail.com>
2023-04-28 17:59:11 +0100

    implement most of trigger response system

--- extensions/trigger_response.py
+++ extensions/trigger_response.py
@@ -1,4 +1,67 @@
 from discord.ext import commands
+import csv
+import random
+
+responses = [
+    "Someone has bad taste in photos",
+    "Your right to live has now been revoked",
+    "I wish I hadn't seen that",
+    "MY EYES! MY EYES! WHY WOULD YOU SEND THAT!?",
+    "Oh my, what have I just witnessed",
+    "That be kinda stanky ngl",
+    "Spare me, please",
+    "I'm not feeling that, delete immediatley",
+    "That is not vibe",
+]
+
+funnies = [
+    "Hmm, maybe I'll laugh too next time",
+    "HAHAHA, was that the reaction you were looking for?",
+    "It would be easier to laugh if it were funny",
+    "That is **h i l a r i o u s**",
+    "L O L",
+    "Wow my dude that is so funny",
+]
+
+answers = [
+    "Good question",
+    "idk",
+    "Why are you asking me?",
+    "I don't think we'll ever know",
+]
+
+WHOLE = 0  # Entire message must match trigger
+START = 1  # Message must start with trigger
+END = 2  # Message must end with trigger
+ANYWHERE = 3  # Message must contain trigger anywhere in the message
+REPEAT = 4  # Message starts and ends with some trigger with any amount of a repeated letter between
+LETTERS = 5  # Message must consist entirely of the trigger repeated (e.g oooooo, hehehe, aaaaa)
+REPLACE = 6  # Message must contain trigger, which will be replaced by the response
+
+trigger_types = ["whole", "start", "end", "anywhere", "repeat", "letters", "replace"]
+
+EXACT = 0
+ANYCASE = 1
+
+cases = ["exact", "anycase"]
+
+entries = list()
+
+
+class entry:
+    def __init__(self, _triggers, _type, _case, _response, _priority):
+        self.triggers = _triggers
+        self.type = trigger_types.index(_type)
+        self.case = cases.index(_case)
+        if self.case == ANYCASE:
+            self.triggers = [t.lower() for t in self.triggers]
+        self.response = _response
+        self.priority = _priority
+
+
+for row in csv.reader(open("data/CSVs/Triggers.csv", "r")):
+    entries.append(entry(row[0].split("\\"), *row[1:]))
+
 
 class TriggerResponse(commands.Cog):
     def __init__(self, bot):
@@ -10,13 +73,93 @@ class TriggerResponse(commands.Cog):
         if message.author.bot:
             return
         # If user is executing a command, don't treat it as a trigger
-        if message.content.split(' ')[0] in self.bot.all_commands:
+        if (
+            message.content.startswith("fbot ")
+            and message.content.split(" ")[1] in self.bot.all_commands.keys()
+        ):
+            return
+        # Check if FBot is enabled in the server
+        c = self.bot.db.cursor()
+        c.execute("SELECT Enabled FROM Guild WHERE id=%s;", (message.guild.id,))
+        enabled = c.fetchone()
+        if enabled is None:
+            # Oopsie! Server isn't in DB. Let's add it, and enable it by default.
+            enabled = (1,)
+            c.execute(
+                "INSERT INTO Guild (id, Enabled) VALUES (%s, %s);",
+                (message.guild.id, 1),
+            )
+        if not enabled[0]:
             return
         # TODO fball
         # TODO respond to "f bot", self.bot.user.id etc with fbot help
         # TODO respond to attachments
-        # TODO implement trigger response lol
+        # TODO implement priorities
         # TODO if response length greater than 2000, trim to first 1997 chars and add ...
+        for entry in entries:
+            msg = message.content.lower() if entry.case == ANYCASE else message.content
+            for trigger in entry.triggers:
+                print(trigger, entry.type == WHOLE and msg == trigger)
+                # All these trigger types are all trivially checked
+                if (
+                    (entry.type == WHOLE and msg == trigger)
+                    or (entry.type == START and msg.startswith(trigger))
+                    or (entry.type == END and msg.endswith(trigger))
+                    or (entry.type == ANYWHERE and trigger in msg)
+                    or (entry.type == LETTERS and msg.replace(trigger, "") == "")
+                    or (entry.type == REPLACE and trigger in msg)
+                ):
+                    break
+                # The REPLACE type requires a bit more code:
+                if entry.type == REPEAT:
+                    for alias in entry.triggers:
+                        start, end = alias.split("~")
+                        letter = start[-1]
+                        if (
+                            msg.startswith(start)
+                            and msg.endswith(end)
+                            and (
+                                all([c == letter for c in msg[len(start) : -len(end)]])
+                            )
+                        ):
+                            trigger = trigger
+                            break
+                    else:
+                        continue
+                    break
+            else:
+                continue
+            break
+        else:
+            await message.channel.send("no triggers")
+            return  # No response
+        response = entry.response.replace("{username}", message.author.display_name)
+        response = response.replace("{answer}", random.choice(answers))
+        response = response.replace("{funny}", random.choice(funnies))
+        response = response.replace("{message}", message.content)
+        response = response.replace("{after}", message.content[len(trigger) :])
+        await message.channel.send(response)
+
+    @commands.command()
+    async def on(self, ctx):
+        c = self.bot.db.cursor()
+        c.execute("SELECT * FROM Guild WHERE id=%s", (ctx.guild.id,))
+        if c.fetchone() is None:
+            c.execute("INSERT INTO Guild (id, Enabled) VALUES (%s, 1)", (ctx.guild.id,))
+        else:
+            c.execute("UPDATE Guild SET Enabled = 1 WHERE id=%s;", (ctx.guild.id,))
+        await ctx.reply("Enabled")
+
+    @commands.command()
+    async def off(self, ctx):
+        c = self.bot.db.cursor()
+        c.execute("SELECT * FROM Guild WHERE id=%s", (ctx.guild.id,))
+        if c.fetchone() is None:
+            c.execute("INSERT INTO Guild (id, Enabled) VALUES (%s, 0)", (ctx.guild.id,))
+        else:
+            c.execute("UPDATE Guild SET Enabled = 0 WHERE id=%s;", (ctx.guild.id,))
+        await ctx.reply("Disabled")
+
 
 async def setup(bot):
     await bot.add_cog(TriggerResponse(bot))
